b9e9c031950c3ad23be1d1b5631a9d7c35fbd083
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-06 21:16:00 +0530

    Update PalindromeRecursion.java

--- Basic recursion/PalindromeRecursion.java
+++ Basic recursion/PalindromeRecursion.java
@@ -1,1 +1,15 @@
-
+class Checker{
+  public boolean function(int i,String s){
+    int n = s.length();
+    if(i >= n/2) return true;
+    if(s.charAt(i) != s.charAt(n-i-1)) return false;
+    return function(i + 1,s);
+  }
+}
+public class PalindromeRecursion{
+  public static void main(String[] args){
+    Checker c = new Checker();
+    String s = "waseem";
+    System.out.println( c.function(0,s));
+  }
+}
