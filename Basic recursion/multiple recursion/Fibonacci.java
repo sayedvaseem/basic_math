c9f1d3e0dfe238f7ca3c6096d64c310dcaa25f73
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-07 21:25:13 +0530

    Update Fibonacci.java

--- Basic recursion/multiple recursion/Fibonacci.java
+++ Basic recursion/multiple recursion/Fibonacci.java
@@ -1,1 +1,14 @@
-
+class MultipleRecursion{
+    int function(int n){
+        if(n <= 1) return n;
+        int last = function(n-1);
+        int slast = function(n-2);
+        return last + slast;
+    }
+}
+class Fibonacci {
+    public static void main(String[] args) {
+        MultipleRecursion mr = new MultipleRecursion();
+        System.out.println(mr.function(4));
+    }
+}
