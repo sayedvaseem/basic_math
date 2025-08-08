7f5727f30cf50810135cbf294e173688b0e6319a
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-08 18:39:05 +0530

    Update FrequencyOfNumbers.java

--- Basic recursion/basic hashing/FrequencyOfNumbers.java
+++ Basic recursion/basic hashing/FrequencyOfNumbers.java
@@ -1,1 +1,22 @@
+import java.util.*;
+public class ReverseOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int q = sc.nextInt();
+        int[] hash = new int[13];
+        for(int i =0;i<n;i++){
+            hash[arr[i]] += 1;
+        }
+        for(int i=0;i<q;i++){
+            int number = sc.nextInt();
+            System.out.println(hash[number]);
+        }
+    }
+}
 
