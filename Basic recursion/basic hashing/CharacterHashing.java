6b2937ed5ac66c702f16dc97f7e453fa62987ca1
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-08 19:11:43 +0530

    Update CharacterHashing.java

--- Basic recursion/basic hashing/CharacterHashing.java
+++ Basic recursion/basic hashing/CharacterHashing.java
@@ -1,1 +1,21 @@
+import java.util.*;
+public class CharacterHashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int[] hash = new int[26];
+
+        for(int i = 0;i<s.length();i++){
+            hash[s.charAt(i) - 'a'] += 1;
+        }
+
+
+        int q = sc.nextInt();
+        for(int i=0;i<q;i++){
+            char c = sc.next().charAt(0);
+            System.out.println(hash[c - 'a']);
+        }
+    }
+}
 
