b2cfd3ceb6452d88521b3f22e1c5a86b801f50e1
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-09 10:07:10 +0530

    Update HashMap.java

--- Basic recursion/basic hashing/HashMap.java
+++ Basic recursion/basic hashing/HashMap.java
@@ -1,1 +1,29 @@
+import java.util.*;
+public class HashMap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int key = arr[i];
+            int freq = 0;
+            if(mp.containsKey(key)) freq = mp.get(key);
+            freq++;
+            mp.put(key, freq);
+        }
+
+        int q = sc.nextInt();
+        for(int i=0;i<q;i++){
+            int number = sc.nextInt();
+            if(mp.containsKey(number)) System.out.println(mp.get(number));
+            else System.out.println(0);
+        }
+    }
+}
 
