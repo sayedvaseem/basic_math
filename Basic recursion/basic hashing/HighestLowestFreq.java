468b2482de26fb3312f86e59483e95674d9d6a75
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-09 19:45:16 +0530

    Update HighestLowestFreq.java

--- Basic recursion/basic hashing/HighestLowestFreq.java
+++ Basic recursion/basic hashing/HighestLowestFreq.java
@@ -1,1 +1,38 @@
+import java.util.*;
+public class HighestLowestFreq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
 
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int key = arr[i];
+            int freq = 0;
+            if(map.containsKey(key)) freq = map.get(key);
+            freq++;
+            map.put(key, freq);
+        }
+        int maxfreq = 0,minfreq = n;
+        int maxele = 0,minele = 0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if(count > maxfreq){
+                maxfreq = count;
+                maxele = element;
+            }
+            if(count < minfreq){
+                minfreq = count;
+                minele = element;
+            }
+        }
+        System.out.println("the hightest frequency element is: " + maxele);
+        System.out.println("the lowest frequency element is: " + minele);
+    }
+}
