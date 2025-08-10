451066b7a9e00fa2ec9a18106b143c977150ffe5
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-10 17:40:58 +0530

    Update SelectionSort.java

--- Sorting-I/SelectionSort.java
+++ Sorting-I/SelectionSort.java
@@ -1,1 +1,28 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i<n-1;i++) {
+            int mini = i;
+            for (int j = i+1;j<n;j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+      for(int i=0;i<n;i++){
+          System.out.print(arr[i] + " ");
+      }
+    }
+}
 
