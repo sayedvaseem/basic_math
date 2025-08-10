c65f423cd009062e83c72c0ab661189e39601dee
sayedvaseem <wisdomwaseem54321@gmail.com>
2025-08-10 18:03:53 +0530

    Update BubbleSort.java

--- Sorting-I/BubbleSort.java
+++ Sorting-I/BubbleSort.java
@@ -10,15 +10,16 @@ public class BubbleSort {
         }
 
         for (int i = 0; i<n-1;i++) {
-            
+            int didSwap = 0;
             for (int j = 0;j<n - i -1;j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = 1;
                 }
             }
-            
+            if(didSwap == 0) break;
         }
       for(int i=0;i<n;i++){
           System.out.print(arr[i] + " ");
